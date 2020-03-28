62fe33734399458189d7b248baaf779c8625b6ab
kxxrxx <keishadaco@gmail.com>
2020-03-28 03:23:56 -0700

    Update rg_toggle2.c

--- rg_toggle2.c
+++ rg_toggle2.c
@@ -1,3 +1,8 @@
+// If SW1 is pressed, the green LED will be toggled every half second (the red LED will be off).
+// If SW1 is not pressed, the red LED will be toggled every half second (the green LED will be off).
+// Whenever SW2 is pressed (no matter SW1 is pressed or not), all LEDs will be off.
+
+
 //*****************************************************************************
 
 #include <stdint.h>
@@ -11,12 +16,13 @@
 #include "driverlib/gpio.h"
 #include "inc/tm4c123gh6pm.h"
 
-#define SW1	     						GPIO_PIN_4		// PF4
-#define SW2      						GPIO_PIN_0		// PF0
+#define SW1	     		GPIO_PIN_4		// PF4
+#define SW2      		GPIO_PIN_0		// PF0
 #define red_LED           	GPIO_PIN_1		// 0x02
-#define green_LED					  GPIO_PIN_3		// 0x08
+#define green_LED		GPIO_PIN_3		// 0x08
 
 //*****************************************************************************
+
 void
 PortFunctionInit(void)
 {
@@ -35,7 +41,7 @@ PortFunctionInit(void)
     //Now modify the configuration of the pins that we unlocked.
     //
 	
-	  //
+    //
     // Enable pin PF0 for GPIOInput
     //
     GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0);
@@ -55,7 +61,7 @@ PortFunctionInit(void)
     //
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_3);
 
-		// ****** Initialized by PinMux Utility *************** 
+// ************** Initialized by PinMux Utility *************** 
 	
     // enable pullup resistors using bitwise OR of PF4(0x10), PF0(0x01)
     GPIO_PORTF_PUR_R |= 0x11;        
@@ -64,11 +70,11 @@ PortFunctionInit(void)
 
 int DelayInSec(double delay_in_s)
 {
-		// SysCtlClockGet() returns the processor clock frequency of the Tiva (16 MHz +/- 1%)
-		// 1 clock cycle (in seconds) = 1 / SysCtlClockGet() second
-		// 1 SysCtlDelay = 3 clock cycles = 3 / SysCtlClockGet() 
-		// 1 second = SysCtlClockGet()  / 3
-		SysCtlDelay(delay_in_s * (SysCtlClockGet()  / 3));
+    // SysCtlClockGet() returns the processor clock frequency of the Tiva (16 MHz +/- 1%)
+    // 1 clock cycle (in seconds) = 1 / SysCtlClockGet() second
+    // 1 SysCtlDelay = 3 clock cycles = 3 / SysCtlClockGet() 
+    // 1 second = SysCtlClockGet()  / 3
+    SysCtlDelay(delay_in_s * (SysCtlClockGet()  / 3));
 }
 	
 
@@ -76,32 +82,32 @@ int main(void)
 {
     uint8_t LED_data;
 	
-		//initialize the GPIO ports	
-		PortFunctionInit();
+    //initialize the GPIO ports	
+    PortFunctionInit();
 	
-		while(1)
+    while(1)
+   {
+	if(GPIOPinRead(GPIO_PORTF_BASE, SW2)==0x00) //SW2 is pressed, all off
+	{
+		GPIOPinWrite(GPIO_PORTF_BASE, red_LED, 0x00);
+		GPIOPinWrite(GPIO_PORTF_BASE, green_LED, 0x00);
+	}
+	else
+	{
+		if(GPIOPinRead(GPIO_PORTF_BASE, SW1)==0x00) //SW1 is pressed, toggle green LED (PF3)
 		{
-					if(GPIOPinRead(GPIO_PORTF_BASE, SW2)==0x00) //SW2 is pressed, all off
-					{
-								GPIOPinWrite(GPIO_PORTF_BASE, red_LED, 0x00);
-								GPIOPinWrite(GPIO_PORTF_BASE, green_LED, 0x00);
-					}
-				  else
-				  {
-								if(GPIOPinRead(GPIO_PORTF_BASE, SW1)==0x00) //SW1 is pressed, toggle green LED (PF3)
-								{
-											GPIOPinWrite(GPIO_PORTF_BASE, red_LED, 0x00);
-									    SysCtlDelay( 2666667);
-											LED_data^=green_LED;	
-											GPIOPinWrite(GPIO_PORTF_BASE, green_LED, LED_data);
-								}
-								else //SW1 is not pressed, toggle red LED (PF3)
-								{
-											GPIOPinWrite(GPIO_PORTF_BASE, green_LED, 0x00);
-											DelayInSec(0.5);
-											LED_data^=red_LED;
-											GPIOPinWrite(GPIO_PORTF_BASE, red_LED, LED_data);
-								}
-					 }
-	   }
+			GPIOPinWrite(GPIO_PORTF_BASE, red_LED, 0x00);
+			SysCtlDelay(2666667);
+			LED_data^=green_LED;	
+			GPIOPinWrite(GPIO_PORTF_BASE, green_LED, LED_data);
+		}
+		else //SW1 is not pressed, toggle red LED (PF3)
+		{
+			GPIOPinWrite(GPIO_PORTF_BASE, green_LED, 0x00);
+			DelayInSec(0.5);
+			LED_data^=red_LED;
+			GPIOPinWrite(GPIO_PORTF_BASE, red_LED, LED_data);
+		}
+	}
+    }
 }
